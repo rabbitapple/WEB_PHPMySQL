6d0c95099022477e00e4058fb4a060d635d32a2c
rabbitapple <buzairabbit@gmail.com>
2023-05-27 19:10:32 +0900

    Changed the file upload function and added the file upload function to writing posts.

--- file_up1.php
+++ file_up1.php
@@ -1,9 +1,12 @@
+<!-- file_up1.php -->
+
 <?php
+
 $targetDir = "uploads/"; // 업로드된 파일이 저장될 디렉토리 경로
 require_once "tool/chack_er.php";
 
 // 파일이 업로드되었는지 확인
-if(isset($_POST["submit"])) {
+if(!isset($_POST["submit"])) {
     $targetFile = $targetDir . basename($_FILES["fileToUpload"]["name"]); // 업로드된 파일의 경로와 이름, basenaem함수는 파일경로에서 파일명만 가져옴.    
     $uploadOk = 1; // 파일 업로드가 성공적으로 이루어졌는지 여부를 나타내는 변수
 
@@ -17,39 +20,43 @@ if(isset($_POST["submit"])) {
 
     // 파일 업로드 실행
     if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-    } else {
+        echo "<script> alert('error [0]'); 
+        window.history.back();
+        </script>";
+    } 
+
+    // 업로드된 파일의 정보를 MySQL 데이터베이스에 저장하는 코드 작성
+    $filename = basename($_FILES["fileToUpload"]["name"]);
+    $filesize = $_FILES["fileToUpload"]["size"];
+    $uploader = $_SESSION["name"];
+    $boardNO = "1";
+    $contentNO = $id; //upload_content_1.php에서 정의
+
+
+
+    // MySQL 데이터베이스 연결 및 파일 정보 저장
+    require_once('tool/db_conn.php'); 
+
+
+    // 파일 정보를 데이터베이스 테이블에 삽입하는 SQL 쿼리 실행
+    $sql = "INSERT INTO board1_file (filename, filesize, uploader, uploadDate, boardNO, contentNO) VALUES ('$filename', '$filesize', '$uploader', NOW(), '$boardNO', '$contentNO')";
+
+    if (mysqli_query($con, $sql)) {
+        $idName = mysqli_insert_id($con); // AUTO_INCREMENT로 생성된 primary key 값을 가져옴
+        echo "<script> alert('Success');
+        </script> ";
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
             // 파일 업로드 성공
-            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+            echo "<script> alert('The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.');
+            window.location.href = '/nk/board1.php';</script>";        
             
-            // 업로드된 파일의 정보를 MySQL 데이터베이스에 저장하는 코드 작성
-            $filename = basename($_FILES["fileToUpload"]["name"]);
-            $filesize = $_FILES["fileToUpload"]["size"];
-            
-
-            // MySQL 데이터베이스 연결 및 파일 정보 저장
-            require_once "tool/db_conn";
-
-            if ($con->connect_error) {
-                die("Connection failed: " . $con->connect_error);
-            }
-
-            // 파일 정보를 데이터베이스 테이블에 삽입하는 SQL 쿼리 실행
-            $sql = "INSERT INTO board1_file (filename, filesize) VALUES ('$filename', '$filesize')";
-            if ($con->query($sql) === TRUE) {
-                echo "File information has been saved to the database.";
-            } else {
-                echo "Error: " . $sql . "<br>" . $con->error;
-            }
-
-            // MySQL 연결 종료
-            $con->close();
-
-        } else {
-            // 파일 업로드 실패
-            echo "Sorry, there was an error uploading your file.";
         }
     }
+        
+    // MySQL 연결 종료
+    $con->close();
 }
+
+
+    
 ?>
